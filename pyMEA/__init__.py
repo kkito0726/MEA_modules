8aab20fa032e8e468d5c7c13c60712b6da9cbeca
kkito0726 <k.kito0726@gmail.com>
2023-03-19 22:18:31 +0900

    importエラーを修正

--- pyMEA/__init__.py
+++ pyMEA/__init__.py
@@ -1,7 +1,7 @@
-from .read_bio import hed2array
-from .peak_detection import detect_peak_neg, detect_peak_pos
-from .plot import showAll, showDetection
-from .gradient import draw, calc_velocity_from_grid
+from pyMEA.read_bio import hed2array
+from pyMEA.peak_detection import detect_peak_neg, detect_peak_pos
+from pyMEA.plot import showAll, showDetection
+from pyMEA.gradient import draw, calc_velocity_from_grid
 
 __all__ = [
   hed2array,
