061eced9747d89a49b2a612f7fcc23113ebb9598
kkito0726 <k.kito0726@gmail.com>
2023-04-12 17:07:14 +0900

    import * の関数を増やした

--- pyMEA/__init__.py
+++ pyMEA/__init__.py
@@ -2,6 +2,7 @@ from pyMEA.read_bio import hed2array
 from pyMEA.peak_detection import detect_peak_neg, detect_peak_pos
 from pyMEA.plot import showAll, showDetection
 from pyMEA.gradient import draw, calc_velocity_from_grid
+from pyMEA.fit_gradient import remove_fit_data, draw_2d, draw_3d
 
 __all__ = [
   'hed2array',
@@ -10,6 +11,9 @@ __all__ = [
   'showAll',
   'showDetection',
   'draw',
-  'calc_velocity_from_grid'
+  'calc_velocity_from_grid',
+  'remove_fit_data',
+  'draw_2d',
+  'draw_3d'
 ]
 
