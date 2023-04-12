e93d0c604755617ba8c1c8f379a36917e0d985d1
kkito0726 <k.kito0726@gmail.com>
2023-04-12 17:02:12 +0900

    コメントの追加

--- pyMEA/fit_gradient.py
+++ pyMEA/fit_gradient.py
@@ -40,12 +40,13 @@ def get_mesh(ele_dis: int, mesh_num: int):
   
   return xx, yy
 
+# この三次関数(2変数)に測定値をフィッティングする。
 def model(X, p00, p10, p01, p20, p11, p02, p30, p21, p12, p03):
   x, y = X
   z = p00 + p10*x + p01*y + p20*x**2 + p11*x*y + p02*y**2 + p30*x**3 + p21*x**2*y + p12*x*y**2 + p03*y**3
   return z.ravel()
 
-
+# フィッティング関数
 def fit_data(times: ndarray, remove_ch, ele_dis: int) -> List[ndarray]:
   xx, yy = get_mesh(ele_dis, 8)
   xx = np.delete(xx, remove_ch)
@@ -59,21 +60,27 @@ def fit_data(times: ndarray, remove_ch, ele_dis: int) -> List[ndarray]:
   return popts
 
 def remove_fit_data(data: ndarray, peak_index: ndarray, ele_dis: int) -> List[ndarray]:
+  # ピーク抽出できなかった電極のデータは除去する
   times, remove_ch = remove_undetected_ch(data, peak_index)
   return fit_data(times, remove_ch, ele_dis)
 
+# 2dカラーマップを描画
 def draw_2d(popts: List[ndarray], ele_dis: int, mesh_num: int, xlabel="", ylabel="", dpi=300) -> None:
+  # grid配列を生成
   xx, yy = get_mesh(ele_dis, mesh_num)
   ex, ey = get_mesh(ele_dis, 8)
   
   for popt in popts:
+    # フィッティングした関数からデータを生成
     z = model([xx, yy], *popt)
     z -= np.min(z)
     z *= 1000
     
+    # 電極上の勾配を算出する
     z_ele = model([ex, ey], *popt)
     grady, gradx = np.gradient(z_ele.reshape(8, 8), ele_dis*7/(8-1)*10**-6)
     
+    # グラフにプロットする
     fig = plt.figure(dpi=dpi)
     ax= fig.add_subplot(111)
     ax.set_aspect('equal', adjustable='box')
@@ -87,15 +94,18 @@ def draw_2d(popts: List[ndarray], ele_dis: int, mesh_num: int, xlabel="", ylabel
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.show()
-    
+
+# 3dカラーマップを描画
 def draw_3d(popts, ele_dis, mesh_num, xlabel="", ylabel="", dpi=300) -> None:
   xx, yy = get_mesh(ele_dis, mesh_num)
   
   for popt in popts:
+    # フィッティングした関数からデータを生成
     z = model([xx, yy], *popt)
     z -= np.min(z)
     z *= 1000
     
+    # グラフにプロットする
     fig = plt.figure(dpi=dpi)
     ax = fig.add_subplot(111, projection='3d')
     c = ax.plot_surface(xx, yy, z.reshape(mesh_num, mesh_num), cmap='jet')
@@ -105,7 +115,8 @@ def draw_3d(popts, ele_dis, mesh_num, xlabel="", ylabel="", dpi=300) -> None:
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     fig.show()
-    
+
+# 伝導速度の算出
 def calc_velocity(popts, ele_dis, mesh_num=8):
   xx, yy = get_mesh(ele_dis, mesh_num)
   
