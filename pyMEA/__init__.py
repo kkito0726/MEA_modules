6358d301e66532a92841aa09edb32dec4fcd2fa1
kkito0726 <k.kito0726@gmail.com>
2023-06-18 07:45:37 +0000

    オートインポート内容を変更

--- pyMEA/__init__.py
+++ pyMEA/__init__.py
@@ -1,19 +1,16 @@
 from pyMEA.read_bio import hed2array
 from pyMEA.peak_detection import detect_peak_neg, detect_peak_pos
 from pyMEA.plot import showAll, showDetection
-from pyMEA.gradient import draw, calc_velocity_from_grid
-from pyMEA.fit_gradient import remove_fit_data, draw_2d, draw_3d
+from pyMEA.fit_gradient import remove_fit_data, draw_2d, draw_3d, calc_velocity
 
 __all__ = [
-  'hed2array',
-  'detect_peak_neg',
-  'detect_peak_pos',
-  'showAll',
-  'showDetection',
-  'draw',
-  'calc_velocity_from_grid',
-  'remove_fit_data',
-  'draw_2d',
-  'draw_3d'
+    "hed2array",
+    "detect_peak_neg",
+    "detect_peak_pos",
+    "showAll",
+    "showDetection",
+    "remove_fit_data",
+    "draw_2d",
+    "draw_3d",
+    "calc_velocity",
 ]
-
