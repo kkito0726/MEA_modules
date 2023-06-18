b011aa7f35c177c6b51eefbc62c14fc9285a5e68
kkito0726 <k.kito0726@gmail.com>
2023-06-18 07:42:55 +0000

    edit version

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pyMEA',  # パッケージ名（pip listで表示される）
-    version="1.1.0",  # バージョン
+    name="pyMEA",  # パッケージ名（pip listで表示される）
+    version="1.1.1",  # バージョン
     description="MEA計測データを読み込み・解析するためのモジュール",  # 説明
-    author='kentaro kito',  # 作者名
+    author="kentaro kito",  # 作者名
     packages=find_packages(),  # 使うモジュール一覧を指定する
-    license='MIT'  # ライセンス
+    license="MIT",  # ライセンス
 )
