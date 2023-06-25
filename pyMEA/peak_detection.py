35e8a102c7eddf550444d9577c8a6a0fc7da00ec
kkito0726 <k.kito0726@gmail.com>
2023-06-25 15:38:22 +0000

    上下のピーク抽出関数を実装

--- pyMEA/peak_detection.py
+++ pyMEA/peak_detection.py
@@ -45,3 +45,43 @@ def detect_peak_pos(
     peak_index[0] = np.array([])
 
     return peak_index
+
+
+# 64電極すべての上下ピークを取得
+def detect_peak_all(
+    MEA_data: ndarray, threshold=[3, 3], distance=3000, width=None, prominence=None
+) -> ndarray:
+    peak_index = np.array([None for _ in range(len(MEA_data))])
+    for i in range(1, 65):
+        # +のデータだけでSDを計算してthresholdを決定する
+        pos_data = MEA_data[i].copy()
+        pos_data[pos_data < 0] = 0
+        pos_height = np.std(pos_data) * threshold[0]
+        # ピーク抽出
+        peak_pos, _ = find_peaks(
+            MEA_data[i],
+            height=pos_height,
+            distance=distance,
+            width=width,
+            prominence=prominence,
+        )
+
+        # -のデータだけでSDを計算してthresholdを決定する
+        neg_data = MEA_data[i].copy()
+        neg_data[neg_data > 0] = 0
+        neg_height = np.std(neg_data) * threshold[1]
+        # ピーク抽出
+        peak_neg, _ = find_peaks(
+            -MEA_data[i],
+            height=neg_height,
+            distance=distance,
+            width=width,
+            prominence=prominence,
+        )
+
+        peak_index[i] = np.append(peak_pos, peak_neg)
+        peak_index[i] = np.sort(peak_index[i])
+
+    peak_index[0] = np.array([])
+
+    return peak_index
