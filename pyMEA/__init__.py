05fae63f94b774fd4051c814defcfe6372935aae
kkito0726 <k.kito0726@gmail.com>
2023-08-17 06:20:04 +0000

    セットアップファイルの編集

--- pyMEA/__init__.py
+++ pyMEA/__init__.py
@@ -1,10 +1,12 @@
 from pyMEA.read_bio import hed2array
+from pyMEA.filter import filter_fft
 from pyMEA.peak_detection import detect_peak_neg, detect_peak_pos
 from pyMEA.plot import showAll, showDetection
 from pyMEA.fit_gradient import remove_fit_data, draw_2d, draw_3d, calc_velocity
 
 __all__ = [
     "hed2array",
+    "filter_fft",
     "detect_peak_neg",
     "detect_peak_pos",
     "showAll",
