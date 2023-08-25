8c2f54dcc29126fc733a9cb1f55bb2b559d1d164
kkito0726 <k.kito0726@gmail.com>
2023-08-25 10:35:19 +0000

    edit setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyMEA",  # パッケージ名（pip listで表示される）
-    version="1.2.1",  # バージョン
+    version="1.2.2",  # バージョン
     description="MEA計測データを読み込み・解析するためのモジュール",  # 説明
     author="kentaro kito",  # 作者名
     packages=find_packages(),  # 使うモジュール一覧を指定する
