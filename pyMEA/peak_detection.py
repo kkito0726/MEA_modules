4515abb1f257ca1b4038f7331b301f72b53aa901
kkito0726 <k.kito0726@gmail.com>
2023-09-18 05:43:43 +0000

    最低閾値ラインを設定し、stdの倍数を引数に設定した

--- pyMEA/peak_detection.py
+++ pyMEA/peak_detection.py
@@ -5,11 +5,15 @@ from scipy.signal import find_peaks
 
 # 64電極すべての下ピークを取得
 def detect_peak_neg(
-    MEA_data: ndarray, distance=5000, width=None, prominence=None
+    MEA_data: ndarray, distance=5000, threshold=3 ,min_amp=10, width=None, prominence=None
 ) -> ndarray:
     peak_index = np.array([None for _ in range(len(MEA_data))])
     for i in range(1, len(MEA_data)):
-        height = np.std(MEA_data[i]) * 3
+        # ピーク抽出の閾値を設定
+        height = np.std(MEA_data[i]) * threshold
+        # 閾値が最低閾値を下回っていた場合は最低閾値の値を閾値の値に設定する
+        if height < min_amp:
+            height = min_amp
         detect_peak_index = find_peaks(
             -MEA_data[i],
             height=height,
