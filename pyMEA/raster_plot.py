d441dba96a0bfac232aa2c48aad485e43a4b1672
kkito0726 <k.kito0726@gmail.com>
2023-11-06 13:14:50 +0000

    一部の電極のみプロットできるようにした

--- pyMEA/raster_plot.py
+++ pyMEA/raster_plot.py
@@ -4,10 +4,10 @@ from numpy import ndarray
 
 
 def raster_plot(
-    MEA_data: ndarray, peak_index: ndarray, figsize=(8, 8), start=0, end=120, xtick=30
+    MEA_data: ndarray, peak_index: ndarray, start_ch=1, end_ch=64, figsize=(8, 8), start=0, end=120, xtick=30
 ) -> None:
     plt.figure(figsize=figsize, dpi=300)
-    for i in range(1, len(MEA_data)):
+    for i in range(start_ch, end_ch+1):
         plt.plot(
             MEA_data[0][peak_index[i]],
             np.ones(len(peak_index[i])) * i,
@@ -15,11 +15,10 @@ def raster_plot(
             color="black",
             markersize=4,
         )
-    # plt.title("Positive peaks & Negative peaks")
     # plt.xticks(range(0, int(np.max(MEA_data[0])), xtick))
     plt.xlim(start, end)
-    plt.ylim(0, 65)
-    plt.yticks(range(4, 65, 4))
+    plt.ylim(start_ch-1, end_ch+1)
+    plt.yticks(range(start_ch, end_ch+1, 4))
     plt.xlabel("Time (s)")
     plt.ylabel("Electrode Number")
     plt.show()
