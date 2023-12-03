5d8bf47086b051db49846891c8af5472537a47b9
kkito <k.kito0726@gmail.com>
2023-12-03 13:06:20 +0000

    showDetectionを追加

--- pyMEA/MEA.py
+++ pyMEA/MEA.py
@@ -2,46 +2,49 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from pyMEA.read_bio import decode_hed, hed2array
+from pyMEA.plot import showDetection
 from numpy import ndarray
 
 
 class MEA:
-    def __init__(self, hed_path: str, start: int=0, end: int=120) -> None:
+    def __init__(self, hed_path: str, start: int = 0, end: int = 120) -> None:
         self.hed_path: str = hed_path
         self.start: int = start
         self.end: int = end
         self.time: int = end - start
         self.SAMPLING_RATE, self.GAIN = decode_hed(self.hed_path)
         self.array: ndarray = hed2array(self.hed_path, self.start, self.end)
-    
+
     def __repr__(self) -> ndarray:
         return repr(self.array)
-    
+
     def __getitem__(self, index: int) -> ndarray:
         return self.array[index]
-    
+
     def __len__(self) -> int:
         return len(self.array)
-    
+
     def __add__(self, value):
         return self.array + value
-    
+
     def __sub__(self, value):
         return self.array - value
-    
+
     def __mul__(self, value):
         return self.array * value
-    
+
     def __truediv__(self, value):
         return self.array / value
-    
+
     def __floordiv__(self, value):
         return self.array // value
-        
+
     @property
     def info(self) -> None:
-        print(f"読み込み開始時間  : {self.start} s\n読み込み終了時間  : {self.end} s\n読み込み合計時間  : {self.time} s\nサンプリングレート: {self.SAMPLING_RATE} Hz\nGAIN           : {self.GAIN}")
-        
+        print(
+            f"読み込み開始時間  : {self.start} s\n読み込み終了時間  : {self.end} s\n読み込み合計時間  : {self.time} s\nサンプリングレート: {self.SAMPLING_RATE} Hz\nGAIN           : {self.GAIN}"
+        )
+
     def showAll(
         self, start=None, end=5, volt_min=-200, volt_max=200, figsize=(8, 8), dpi=300
     ) -> None:
@@ -50,7 +53,7 @@ class MEA:
             start = self.start
         if end == None:
             end == start + 5
-        
+
         # 読み込み開始時間が0ではないときズレが生じるため差を取っている
         start_frame = int(abs(self.start - start) * self.SAMPLING_RATE)
         end_frame = int(abs(self.start - end) * self.SAMPLING_RATE)
@@ -58,7 +61,10 @@ class MEA:
         plt.figure(figsize=figsize, dpi=dpi)
         for i in range(1, 65, 1):
             plt.subplot(8, 8, i)
-            plt.plot(self.array[0][start_frame:end_frame], self.array[i][start_frame:end_frame])
+            plt.plot(
+                self.array[0][start_frame:end_frame],
+                self.array[i][start_frame:end_frame],
+            )
             plt.ylim(volt_min, volt_max)
 
         plt.show()
@@ -66,8 +72,8 @@ class MEA:
     def showSingle(
         self,
         ch: int,
-        start: int =None,
-        end: int =None,
+        start: int = None,
+        end: int = None,
         volt_min=-200,
         volt_max=200,
         figsize=(8, 2),
@@ -80,8 +86,8 @@ class MEA:
             start = self.start
         if end == None:
             end = self.end
-            
-         # 読み込み開始時間が0ではないときズレが生じるため差を取っている
+
+        # 読み込み開始時間が0ではないときズレが生じるため差を取っている
         start_frame = int(abs(self.start - start) * self.SAMPLING_RATE)
         end_frame = int(abs(self.start - end) * self.SAMPLING_RATE)
 
@@ -95,3 +101,30 @@ class MEA:
         plt.ylabel(ylabel)
 
         plt.show()
+
+    def showDetection(
+        self,
+        eles: list[int],
+        start=None,
+        end=None,
+        figsize=(12, 12),
+        xlabel="Time (s)",
+        ylabel="Electrode Number",
+        dpi=300,
+    ) -> None:
+        # 時間の設定がなければ全体をプロットするようにする。
+        if start == None:
+            start = self.start
+        if end == None:
+            end = self.end
+        showDetection(
+            MEA_raw=self.array,
+            eles=eles,
+            start=start,
+            end=end,
+            sampling_rate=self.SAMPLING_RATE,
+            figsize=figsize,
+            xlabel=xlabel,
+            ylabel=ylabel,
+            dpi=dpi,
+        )
