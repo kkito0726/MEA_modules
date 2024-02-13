eab5faec0a4d1a84fb7f5f5602379732d8d9fba3
kkito <k.kito0726@gmail.com>
2024-02-13 16:31:09 +0000

    引数の型を指定

--- pyMEA/peak_detection.py
+++ pyMEA/peak_detection.py
@@ -5,7 +5,12 @@ from scipy.signal import find_peaks
 
 # 64電極すべての下ピークを取得
 def detect_peak_neg(
-    MEA_data: ndarray, distance=5000, threshold=3 ,min_amp=10, width=None, prominence=None
+    MEA_data: ndarray,
+    distance=5000,
+    threshold=3,
+    min_amp=10,
+    width=None,
+    prominence=None,
 ) -> ndarray:
     peak_index = np.array([None for _ in range(len(MEA_data))])
     for i in range(1, len(MEA_data)):
@@ -31,7 +36,11 @@ def detect_peak_neg(
 
 # 64電極すべての上ピークを取得
 def detect_peak_pos(
-    MEA_data: ndarray, distance=10000, width=None, prominence=None, height=(10, 80)
+    MEA_data: ndarray,
+    distance=10000,
+    width=None,
+    prominence=None,
+    height: tuple[int] = (10, 80),
 ) -> ndarray:
     peak_index = np.array([None for _ in range(len(MEA_data))])
     for i in range(1, len(MEA_data)):
@@ -53,7 +62,11 @@ def detect_peak_pos(
 
 # 64電極すべての上下ピークを取得
 def detect_peak_all(
-    MEA_data: ndarray, threshold=[3, 3], distance=3000, width=None, prominence=None
+    MEA_data: ndarray,
+    threshold: list[int] = [3, 3],
+    distance=3000,
+    width=None,
+    prominence=None,
 ) -> ndarray:
     peak_index = np.array([None for _ in range(len(MEA_data))])
     for i in range(1, 65):
