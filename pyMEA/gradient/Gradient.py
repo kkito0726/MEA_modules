d1d90e33f01c2673cbfdfe32565e6fedab786649
kkito <k.kito0726@gmail.com>
2024-06-23 15:13:14 +0900

    gradientの計算を実装

---
+++ pyMEA/gradient/Gradient.py
@@ -0,0 +1,84 @@
+from Solver import Solver
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Gradient:
+    def __init__(self, solver: Solver, k=5.0**-9) -> None:
+        self.solver = solver
+        self.grad_x, self.grad_y = self.grad_model((solver.xx, solver.yy), *solver.popt)
+        self.vx, self.vy = self.grad_velocity(k, self.grad_x, self.grad_y)
+
+    def grad_model(self, X, p00, p10, p01, p20, p11, p02, p30, p21, p12, p03):
+        x, y = X
+        grad_x = (
+            p10 + 2 * p20 * x + p11 * y + 3 * p30 * x**2 + 2 * p21 * x * y + p12 * y**2
+        )
+        grad_y = (
+            p01 + p11 * x + 2 * p02 * y + p21 * x**2 + 2 * p12 * x * y + 3 * p03 * y**2
+        )
+        return grad_x, grad_y
+
+    def grad_velocity(self, k, grad_x, grad_y):
+        return k / grad_x, k / grad_y
+
+    def calc_velocity(self):
+        return np.sqrt(self.vx**2 + self.vy**2)
+
+    def draw2d(
+        self,
+        contour=False,
+        isQuiver=True,
+        xlabel="X (μm)",
+        ylabel="Y (μm)",
+        clabel="Δt (ms)",
+        dpi=300,
+        cmap="jet",
+    ) -> None:
+
+        # グラフにプロットする
+        fig = plt.figure(dpi=dpi)
+        ax = fig.add_subplot(111)
+        ax.set_aspect("equal", adjustable="box")
+        if contour:
+            c = ax.contourf(
+                self.solver.xx,
+                self.solver.yy,
+                self.solver.z.reshape(self.solver.mesh_num, self.solver.mesh_num),
+                cmap=cmap,
+            )
+            ax.contour(
+                self.solver.xx,
+                self.solver.yy,
+                self.solver.z.reshape(100, 100),
+                colors="k",
+                linewidths=0.5,
+                linestyles="solid",
+            )
+        else:
+            c = ax.pcolormesh(
+                self.solver.xx,
+                self.solver.yy,
+                self.solver.z.reshape(self.solver.mesh_num, self.solver.mesh_num),
+                cmap=cmap,
+            )
+        # 電極上に速度ベクトルを描画
+        ele_solver = Solver(
+            self.solver.time, self.solver.remove_ch, self.solver.ele_dis, 8
+        )
+        ele_grad_x, ele_grad_y = self.grad_model(
+            (ele_solver.xx, ele_solver.yy), *ele_solver.popt
+        )
+        ele_vx, ele_vy = self.grad_velocity(1, ele_grad_x, ele_grad_y)
+
+        plt.scatter(ele_solver.xx, ele_solver.yy, marker=",", color="grey")
+        if isQuiver:
+            plt.quiver(ele_solver.xx, ele_solver.yy, ele_vx, ele_vy)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        bar = plt.colorbar(c)
+        bar.set_label(clabel)
+        plt.xticks(np.arange(0, self.solver.ele_dis * 7 + 1, self.solver.ele_dis))
+        plt.yticks(np.arange(0, self.solver.ele_dis * 7 + 1, self.solver.ele_dis))
+
+        plt.show()
---
+++ pyMEA/gradient/Solver.py
@@ -0,0 +1,85 @@
+import numpy as np
+from scipy.optimize import curve_fit
+
+
+class Solver:
+    def __init__(self, time, remove_ch: list[int], ele_dis: int, mesh_num=100) -> None:
+        """
+        Args:
+            time: 波形のピーク時刻
+            remove_ch: ピーク抽出できなかった電極
+            ele_dis: 電極間距離
+            mesh_num: 何x何のグリッドでデータを補間するか
+        """
+        self.time = time
+        self.remove_ch = remove_ch
+        self.ele_dis = ele_dis
+        self.mesh_num = mesh_num
+        self.popt, self.r2s = self.fit_data()
+        self.xx, self.yy = self.get_mesh(self.ele_dis, self.mesh_num)
+        self.z = self.calc_z()
+        pass
+
+    def get_mesh(self, ele_dis: int, mesh_num: int):
+        """
+        グリッド配列を取得するメソッド
+        """
+        # データ範囲を取得
+        x_min, x_max = 0, ele_dis * 7
+        y_min, y_max = 0, ele_dis * 7
+
+        # 取得したデータ範囲で新しく座標にする配列を作成
+        x_coord = np.linspace(x_min, x_max, mesh_num)
+        y_coord = np.linspace(y_min, y_max, mesh_num)
+
+        # 取得したデータ範囲で新しく座標にする配列を作成
+        xx, yy = np.meshgrid(x_coord, y_coord)
+
+        # 電極番号順に配列を修正
+        yy = np.rot90(np.rot90(yy))
+
+        return xx, yy
+
+    def model(self, X, p00, p10, p01, p20, p11, p02, p30, p21, p12, p03):
+        x, y = X
+        z = (
+            p00
+            + p10 * x
+            + p01 * y
+            + p20 * x**2
+            + p11 * x * y
+            + p02 * y**2
+            + p30 * x**3
+            + p21 * x**2 * y
+            + p12 * x * y**2
+            + p03 * y**3
+        )
+        return z.ravel()
+
+    def fit_data(self) -> tuple[list[float], float]:
+        """
+        MEAの計測データからmodel式にデータをフィッティングする
+
+        Returns:
+            popt: モデル式の係数が入ったリスト
+            r2: 決定係数
+        """
+        xx, yy = self.get_mesh(self.ele_dis, 8)
+        xx = np.delete(xx, self.remove_ch)
+        yy = np.delete(yy, self.remove_ch)
+
+        popt, _ = curve_fit(self.model, [xx, yy], self.time)
+
+        residuals = self.time - self.model([xx, yy], *popt)
+        rss = np.sum(residuals**2)
+        tss = np.sum((self.time - np.mean(self.time)) ** 2)
+        r2 = 1 - (rss / tss)
+
+        return np.array(popt), r2
+
+    def calc_z(self):
+        z = self.model((self.xx, self.yy), *self.popt)
+        z -= np.min(z)  # 発火起点を0 sとする
+        z *= 1000  # 単位をmsに変更
+
+        return z
