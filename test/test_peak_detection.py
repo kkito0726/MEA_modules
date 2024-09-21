4d44f3e94956cb89c25c52499c8d869551e6c58a
kkito <k.kito0726@gmail.com>
2024-09-21 11:22:41 +0900

    テスト追加

--- test/test_peak_detection.py
+++ test/test_peak_detection.py
@@ -1,5 +1,7 @@
 import unittest
 
+from pyMEA.find_peaks.peak_detection import detect_peak_pos
+
 from pyMEA import MEA, detect_peak_neg
 
 
@@ -7,12 +9,18 @@ class MyTestCase(unittest.TestCase):
     def setUp(self):
         self.path = "./public/230615_day2_test_5s_.hed"
         self.data = MEA(self.path, 0, 5)
-        self.peak_index = detect_peak_neg(self.data.array)
+        self.neg_peak_index = detect_peak_neg(self.data.array)
+        self.pos_peak_index = detect_peak_pos(self.data, height=(200))
 
-    def test_something(self):
+    def test_下方向のピークを抽出できる(self):
         for i in range(1, 65):
-            for value in self.data[i][self.peak_index[i]]:
+            for value in self.data[i][self.neg_peak_index[i]]:
                 self.assertTrue(value < -200)
+
+    def test_上方向のピークを抽出できる(self):
+        for i in range(1, 65):
+            for value in self.data[i][self.pos_peak_index[i]]:
+                self.assertTrue(value > 200)
 
 
 if __name__ == "__main__":
